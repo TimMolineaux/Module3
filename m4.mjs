be23485021a58bd9a2ccbbca5b5a61994e1521f1
TimMolineaux <tim.molineaux@gmail.com>
2024-10-08 11:23:26 +0200

    m4

---
+++ m4.mjs
@@ -0,0 +1,19 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+for (let i = 0; i < 100; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log(`FizzBuzz`);
+    }
+    else if (i % 3 == 0){
+        console.log(`Fizz`);
+    }else if (i % 5 == 0){
+        console.log(`Buzz`);
+    }else{
+        console.log(i);
+    }
+
+}
+
+process.exit();
